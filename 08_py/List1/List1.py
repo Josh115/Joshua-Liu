507ef04df5033d5858e4f93f5d0bafef2d3ad4ad
Joshua Liu <78169638+Josh115@users.noreply.github.com>
2022-09-21 18:36:26 -0400

    Update List1.py

--- 08_py/List1/List1.py
+++ 08_py/List1/List1.py
@@ -54,26 +54,54 @@ reverse3([1, 2, 3]) #→ [3, 2, 1]
 reverse3([5, 11, 9]) #→ [9, 11, 5]
 reverse3([2, 1, 2]) #→ [2, 1, 2]
 
-
+# params: nums- array of ints with length 3
+# Checks to see if the first element is larger than the last, if it is, then set all elements equal to the first element, else set all elements equal to the last.
 def max_end3(nums):
   if nums[0] > nums[-1]:
     nums[0], nums[1], nums[2] = nums[0], nums[0], nums[0]
   else:
     nums[0], nums[1], nums[2] = nums[2], nums[2], nums[2]
-
   return nums
 
+max_end3([1, 2, 3]) #→ [3, 3, 3]
+max_end3([11, 5, 9]) #→ [11, 11, 11]
+max_end3([2, 11, 3]) #→ [3, 3, 3]
+
+# params: nums is an array of ints
+# Add up the first 2 elements of the array. nums[:2] returns an array of the first 2 elements, or just the array itself if it has less than length 2.
 def sum2(nums):
   x = 0
   for i in nums[:2]:
     x += i
   return x
 
+sum2([1]) #→ 1
+sum2([1, 2]) #→ 3
+sum2([1, 1, 1, 1]) #→ 2
+
+# params: a, b - are both int arrays of length 3.
+# return an array containing the middle elements in both arrays
 def middle_way(a, b):
   return [a[1], b[1]]
 
+middle_way([1, 2, 3], [4, 5, 6]) #→ [2, 5]
+middle_way([7, 7, 7], [3, 8, 0]) #→ [7, 8]
+middle_way([5, 2, 9], [1, 4, 5]) #→ [2, 4]
+
+# params: nums is an array of ints
+# return an array with the first and last elements in the array, nums[-1] returns the last element in nums.
 def make_ends(nums):
   return [nums[0], nums[-1]]
 
+make_ends([501, 20, 5]) #→ [501, 5]
+make_ends([1, 2, 3, 1]) #→ [1, 1]
+make_ends([-7, 7]) #→ [-7, 7]
+
+# params: nums - array of length 2
+# 2 in nums checks if nums contains 2. return if nums contains 2 or 3.
 def has23(nums):
   return 2 in nums or 3 in nums
+
+has23([2, 1]) #→ True
+has23([2, 3]) #→ True
+has23([0, 0]) #→ False
