4ebab9840cf089dbfd67e1f8ad646b2d2eff7733
Joshua Liu <jliu31@stuy.edu>
2022-09-29 14:02:30 -0400

    removed code from home

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -10,10 +10,4 @@ import random
 
 Test = "8$$$Joshua Liu$$$Ducky@@@3$$$RandomPerson$$$Duckling@@@100$$$MickeyMouse$$$Cat"
 def choose():
-    Profiles = Test.split("@@@")
-    randomPerson = random.choice(Profiles)
-    Data = randomPerson.split("$$$")
-    return Data[1] + " from period" + Data[0] + " has a ducky named: " + Data[2]
-
-        
-print(choose())
+    
