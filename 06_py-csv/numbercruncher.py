7c43a30f1b30d78f5ab5d16e4bc09c17726dc940
Joshua Liu <jliu31@stuy.edu>
2022-10-02 13:34:06 -0400

    Added a method to parse the info of the csv file to a job and weight list

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,1 +1,32 @@
+'''
+Team:
+DISCO:
+QCC:
+'''
 
+import random
+
+def main():
+    jobs = []
+    weight = []
+    # parseinfo takes in a csv file and 2 lists. It parses the jobs in the csv file to the jobs list and the weights to the weight list, including the total
+    parseinfo("occupations.csv", jobs, weight)
+    
+    
+def parseinfo(file, jobs, weight):
+    occ_wei = open(file, "r").read()
+    
+    # Creates a list of all occupation,weight pairs, excluding the first and last lines, which aren't relevant.
+    occ_wei = occ_wei.split("\n")[1:]
+    
+    for pair in occ_wei:
+        # first see if pair is empty.
+        if(len(pair) != 0):
+            # rsplit() splits from  the left, and the second argument decides how many times to split.
+            job_percent_pair = pair.rsplit(",", 1)
+            # Dont include the Total,percent pair
+            if(job_percent_pair[0] != "Total"):
+                jobs.append(job_percent_pair[0])
+                weight.append(job_percent_pair[1])
+                
+main()
