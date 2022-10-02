81542fe6f36a7833049de05dfa77fd5ef9a8bace
Joshua Liu <jliu31@stuy.edu>
2022-10-02 14:25:05 -0400

    added the rest of the code for main, still haven't tested it yet.

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -6,12 +6,13 @@ QCC:
 
 import random
 
+
 def main():
     jobs = []
     weight = []
     # parseinfo takes in a csv file and 2 lists. It parses the jobs in the csv file to the jobs list and the weights to the weight list, including the total
     parseinfo("occupations.csv", jobs, weight)
-    
+    print(random.choices(jobs, weights = weight))
     
 def parseinfo(file, jobs, weight):
     occ_wei = open(file, "r").read()
@@ -27,6 +28,8 @@ def parseinfo(file, jobs, weight):
             # Dont include the Total,percent pair
             if(job_percent_pair[0] != "Total"):
                 jobs.append(job_percent_pair[0])
-                weight.append(job_percent_pair[1])
+                # Because we are using the choices method, the weights list has to only include integers, since
+                weight.append(int(float(job_percent_pair[1]) * 10))
                 
+
 main()
