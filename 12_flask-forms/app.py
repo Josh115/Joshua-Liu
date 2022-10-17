b9d09838961ec3f74d4265efb1438eb90030cc3d
Joshua Liu <jliu31@stuy.edu>
2022-10-17 18:44:10 -0400

    copied selena's files and modified some things.

---
+++ 12_flask-forms/app.py
@@ -0,0 +1,51 @@
+# Gordon Mo, Joshua Liu, Selena Ho
+# Team: Go Jose!
+# SoftDev
+# K12 -- Take and Give//POST Requests
+# 2022-10-17
+# time spent: .9
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+
+'''
+trioTASK:
+~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
+...read for understanding all of the code below.
+Some will work as written; other sections will not. 
+TASK: Predict which...
+Devise some simple tests you can run to "take apart this engine," as it were.
+Execute your tests.
+Process results.
+
+PROTIP: Insert your own in-line comments
+ wherever they will help
+  your future self and/or current teammates
+   understand what is going on.
+'''
+
+@app.route("/", methods=['GET', 'POST'])
+def disp_loginpage():
+    return render_template( 'login.html' )
+
+
+@app.route("/response", methods=['GET', 'POST'])
+def authenticate():
+    print(request.form)
+    if(request.method == 'POST'):
+        return render_template('response.html', username = request.form.get('username'))  #response to a form submission
+    else:
+        return render_template('response.html', username = request.args.get('username'))
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
