d6dc1ec303e9743aafbee32a669a2be68c6e305a
Joshua Liu <jliu31@stuy.edu>
2022-10-26 19:28:15 -0400

    Copied code from selena's repo.

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -1,7 +1,8 @@
-#Clyde "Thluffy" Sinclair
+#Go Jose!: Gordon Mo, Joshua Liu, Selena Ho
 #SoftDev  
-#skeleton/stub :: SQLITE3 BASICS
-#Oct 2022
+#K18 // creating database and tables, populating tables using python
+#2022-10-26
+#time spent: 1.5
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
@@ -15,16 +16,28 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 #==========================================================
 
-c.execute("create table students(f1)")
-# < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
-with open ("students.csv") as csvfile:
-    Return_dict_reader = csv.DictReader(csvfile)
-    #example row in students: {'name': 'alison', 'age': '23', 'id': '10'}
-    for row in Return_dict_reader:
-        c.executescript("insert into students values(" + row.get('name') + " );")
+c.executescript("""create table students(name text, age int, id int primary key);
+create table courses(code text, mark int, id int);
+""")
 
+with open ('students.csv', newline='') as csvfile: #people often forget to include the close line so with open prevents that from happening
+    studentsDR = csv.DictReader(csvfile)
+    #example row in studentsDR: {'name': 'alison', 'age': '23', 'id': '10'}
+    for entry in studentsDR:
+        name = entry['name']
+        age = entry['age']
+        Student_id = entry['id']
+        Execute_string = f'insert into students values("{name}", {age}, {Student_id})' #uses formatted strings, populates students table
+        c.execute(Execute_string)
 
-
+with open ('courses.csv', newline='') as csvfile:
+    coursesDR = csv.DictReader(csvfile)
+    for entry in coursesDR:
+        code = entry['code']
+        mark = entry['mark']
+        Student_id = entry['id']
+        Execute_string = f'insert into courses values("{code}", {mark}, {Student_id})' #uses formatted strings, populates students table
+        c.execute(Execute_string)
 
 command = ""          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
