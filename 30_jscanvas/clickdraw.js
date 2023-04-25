d0ffd9619cba423b875c6978535b8420bfef0a42
Joshua Liu <jliu31@stuy.edu>
2023-04-25 13:42:51 -0400

    finished the featuress

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -1,6 +1,7 @@
 //retrieve node in DOM via ID
 var c = document.getElementById("slate");
-
+var button = document.getElementById("buttonToggle");
+var clear = document.getElementById("buttonClear");
 //instantiate a CanvasRenderingContext2D object
 var ctx = c.getContext("2d");
 
@@ -12,12 +13,25 @@ var toggleMode = (e) => {
     console.log("toggling...");
     if (mode === "rect") {
         mode = "circ"
+        button.innerHTML = "circ"
     }
     else {
         mode = "rect"
+        button.innerHTML = "rect"
     }
 }
 
+var wipe = function(e){
+    ctx.clearRect(0,0,600,600);
+}
+var draw = function(e){
+    if (mode === "rect") {
+        drawRect(e)
+    }
+    else {
+        drawCircle(e)
+    }
+}
 var drawRect = function(e) {
     var mouseX = e.clientX - c.offsetLeft + window.pageXOffset //gets X-coor of mouse when event is fired and offsets it based on the position of the canvas and the offset of the page(scrolling through the pages)
     var mouseY = e.clientY - c.offsetTop + window.pageYOffset //gets Y-coor of mouse when event is fired
@@ -27,7 +41,11 @@ var drawRect = function(e) {
 var drawCircle = function(e){
     var mouseX = e.clientX - c.offsetLeft + window.pageXOffset //gets X-coor of mouse when event is fired and offsets it based on the position of the canvas and the offset of the page(scrolling through the pages)
     var mouseY = e.clientY - c.offsetTop + window.pageYOffset //gets Y-coor of mouse when event is fired
-    ctx.fill(mouseX, mouseY, 10, 10)
+    ctx.beginPath();
+    ctx.arc(mouseX, mouseY, 10, 0, 2*Math.PI)
+    ctx.stroke();
+    ctx.fill();
 }
-
-c.addEventListener("click", drawRect) //passes the mouse event as parameter for the function
+button.addEventListener("click", toggleMode)
+clear.addEventListener("click", wipe)
+c.addEventListener("click", draw) //passes the mouse event as parameter for the function
